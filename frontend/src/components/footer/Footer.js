2ff606ab4b24e59fdf443d74345a6781c0b0738b
Arman Garg <armangarg36@gamil.com>
2023-06-08 19:20:00 +0530

    .

---
+++ frontend/src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './footer.css'
+
+export default function Footer() {
+  return (
+    <>
+        <div className='footercontainer'>
+            <div>
+                footer left
+            </div>
+
+            <div>
+                footer right
+            </div>
+        </div>
+    </>
+  )
+}
