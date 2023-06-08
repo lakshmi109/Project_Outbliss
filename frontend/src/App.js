b36ecff52f71e66808f329f413f7bb251750043a
Arnav Singhal <94188860+arnavvsinghal@users.noreply.github.com>
2023-06-08 19:44:29 +0530

    Added Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 
+// import { NativeBuffer } from 'mongoose';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Footer/>
     </>
   );
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+// import { ReactDOM } from 'react';
+import './Navbar.css';
+import Person from './images/person.svg';
+import ShoppingCart from './images/shopping cart.svg';
+import Hamburger from './images/hamburger.svg';
+class Navbar extends Component {
+    render() {
+        return (
+            <div className="body">
+                <div className="header">
+                    <ul className="navBar">
+                        <li>
+                            <ul className="navLeft">
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Products</a></li>
+                                <li><a href="">Contact</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <ul className="navRight">
+                                <li><a href=""><img src={Person} alt="Person" /></a></li>
+                                <li><a href=""><img src={ShoppingCart} alt="Shopping Cart" /></a></li>
+                                <li><a href=""><img src={Hamburger} alt="Hamburger" /></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
