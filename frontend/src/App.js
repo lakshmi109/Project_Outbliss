ccf163c10bda0187fc0a1eeedc0a6ed21415b0dd
Arnav Singhal <94188860+arnavvsinghal@users.noreply.github.com>
2023-06-10 00:09:18 +0530

    testimonial structure added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/footer/Footer';
 import SearchBox from './components/searchBox/SearchBox';
 import BestSelling from './components/bestselling/BestSelling';
 import Aboutus from './components/aboutus/Aboutus';
+import Testimonialbox from './components/testimonialbox/Testimonialbox';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <SearchBox/>
       <BestSelling/>
       <Aboutus/>
+      <Testimonialbox/>
       <Footer/>
     </>
   );
